47af9df0c90dddeae1c3e67f48f8217986ee9056
Ian <iansunlit@outlook.com>
2020-10-01 01:28:06 -0600

    Completed section on variables

---
+++ variablesScript.js
@@ -0,0 +1,14 @@
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+let nameOfOurPlanet = "Earth";
+let nameOfCurrentVisitor = "Tom";
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+
